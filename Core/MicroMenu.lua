fb157be22b633f66b29a400189fe04526c707ff9
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-02-09 13:01:12 +0200

    Update comment

--- Core/MicroMenu.lua
+++ Core/MicroMenu.lua
@@ -2,9 +2,9 @@ local _, addon = ...
 local MicroMenu = {}
 addon.MicroMenu = MicroMenu
 
--- NOTE: 
--- Sometimes the 'AchievementMicroButton' tooltip wouldn't show up because 'tooltipText' is nil, 
---  I'm not sure why or whether this is a Blizzard bug but this aims to fix that.
+-- NOTE: The `AchievementMicroButton` tooltip sometimes doesn't show up because `tooltipText` is nil.
+--       This might be a Blizzard bug. This function aims to fix that issue by setting the tooltip text if it's missing.
+--       This is essential for `WowInfo` because without the tooltip text, the tooltip won't show up.
 function MicroMenu:SetButtonTooltip(button, text, action)
     if button:IsEnabled() then
         if not button.tooltipText then
