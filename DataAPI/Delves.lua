c8bb74685040c41299ff6476966cf32e1e74e8e0
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-03-05 21:18:14 +0200

    Fix Delves when GetFactionForCompanion returns 0

--- DataAPI/Delves.lua
+++ DataAPI/Delves.lua
@@ -6,7 +6,9 @@ local INFO = {
 }
 
 local CACHE = {
-    Renown = {},
+    Renown = {
+        currentValue = -1
+    },
     Companion = {}
 }
 
@@ -15,54 +17,58 @@ local MIN_REP_THRESHOLD_BAR_VALUE = MIN_REP_RANK_FOR_REWARDS - 1
 local MAX_REP_RANK_FOR_REWARDS = 10
 
 local function CacheFactionData()
-    local renownInfo = C_MajorFactions.GetMajorFactionRenownInfo(C_DelvesUI.GetDelvesFactionForSeason())
-    local renownLevel = renownInfo.renownLevel
-    local value = renownInfo.renownReputationEarned / renownInfo.renownLevelThreshold
-
-    local companionFactionID = C_DelvesUI.GetFactionForCompanion(Constants.DelvesConsts.BRANN_COMPANION_INFO_ID)
-    local companionFactionInfo = C_Reputation.GetFactionDataByID(companionFactionID)
-    local friendshipData = C_GossipInfo.GetFriendshipReputation(companionFactionID)
-
-    CACHE.Renown.currentValue = renownInfo and (RoundToSignificantDigits(renownInfo.renownLevel + value, 1)) or 0
+    local companionFactionID = C_DelvesUI.GetFactionForCompanion()
+    if companionFactionID == 0 then return end
     
-    CACHE.Companion.name = companionFactionInfo.name
-
+    local friendshipData = C_GossipInfo.GetFriendshipReputation(companionFactionID)
+   
     if friendshipData and friendshipData.friendshipFactionID > 0 then
+        local companionFactionInfo = C_Reputation.GetFactionDataByID(companionFactionID)
+        
+        CACHE.Companion.name = companionFactionInfo.name
+
         local rankInfo = C_GossipInfo.GetFriendshipReputationRanks(friendshipData.friendshipFactionID)
+
         if rankInfo.maxLevel > 0 then
             CACHE.Companion.currentLevel = rankInfo.currentLevel
             CACHE.Companion.maxLevel = rankInfo.maxLevel
         end
+
+        local renownInfo = C_MajorFactions.GetMajorFactionRenownInfo(C_DelvesUI.GetDelvesFactionForSeason())
+        local renownLevel = renownInfo.renownLevel
+        local value = renownInfo.renownReputationEarned / renownInfo.renownLevelThreshold
+    
+        CACHE.Renown.currentValue = renownInfo and (RoundToSignificantDigits(renownInfo.renownLevel + value, 1)) or 0
     else
         CACHE.Companion.currentLevel = 0
         CACHE.Companion.maxLevel = 0
+        CACHE.Renown.currentValue = -1
 	end
 end
 
-Delves:RegisterEvent("PLAYER_LOGIN", function(self, eventName)
-    CacheFactionData()
-    self:RegisterEvents(
-        "MAJOR_FACTION_RENOWN_LEVEL_CHANGED", 
-        "MAJOR_FACTION_UNLOCKED",
-        "UPDATE_FACTION", CacheFactionData)
-end)
+Delves:RegisterEvents(
+    "MAJOR_FACTION_RENOWN_LEVEL_CHANGED", 
+    "MAJOR_FACTION_UNLOCKED",
+    "UPDATE_FACTION", CacheFactionData)
 
 function Delves:GetProgressInfo()
     local seasonNumber = C_DelvesUI.GetCurrentDelvesSeasonNumber()
     local isActiveSeason = seasonNumber and seasonNumber > 0
 
-    if isActiveSeason and CACHE.Renown.currentValue > 0 then
+    if isActiveSeason and CACHE.Renown.currentValue > -1 then
         local currExpID = GetExpansionLevel()
-        local expName = _G["EXPANSION_NAME"..currExpID]
+        local expName = _G["EXPANSION_NAME" .. currExpID]
 
         INFO.seasonNumber = seasonNumber
         INFO.expansion = expName
-        INFO.currentValue = CACHE.Renown.currentValue
-        INFO.maxValue = MAX_REP_RANK_FOR_REWARDS
         INFO.companionName = CACHE.Companion.name
         INFO.companionLevel = CACHE.Companion.currentLevel
         INFO.companionMaxLevel = CACHE.Companion.maxLevel
+        INFO.currentValue = CACHE.Renown.currentValue
+        INFO.maxValue = MAX_REP_RANK_FOR_REWARDS
 
         return INFO
     end
+
+    return nil
 end
